d01e692b602fe320eb1faa3b986f93eb079258a6
Patitobi <kesseltobias0@gmail.com>
2022-10-31 23:06:48 +0100

    x

--- RW2022/main.cpp
+++ RW2022/main.cpp
@@ -1,18 +1,27 @@
 #include <wiringpi.h>
 //#include <studio.h>
-//#include <chrono>
 
 class IRsenden{
     private:
+    int senderPin;
+
     public:
-    IRsenden(){
+    IRsenden(int pin){
+        senderPin=pin;
+        
+    }
+    void sendinfo(){
 
     }
 };
 class IRemfpaenger{
     private:
+        int resiverPin;
     public:
-    IRemfpaenger(){
+    IRemfpaenger(int resiver){
+        resiverPin=resiver;
+    }
+    int getinfo(){
 
     }
 };
@@ -24,13 +33,17 @@ class Reifen{
         Reifen(int pin){
             steuerpin=pin;
             mode=false;
+
+            pinMode(steuerpin, OUTPUT);
         }
         void setmode(bool mode){
             if (mode){
                 //pin auf go setzen für losfahren
+                digitalWrite(steuerpin, HIGH);
                 mode=true;
             } else {
                 //pin auf stop setzen für stopen
+                digitalWrite(steuerpin, LOW);
                 mode=false;
             }
         }
@@ -61,26 +74,32 @@ class Ultraschall{
             float startTimer;
             float stoptimer;
 
-            digitalWrite(trigger, 1);
-            delay(0.1);
-            digitalWrite(trigger, 0);
+            digitalWrite(trigger, HIGH);
+            delayMicroseconds(0.01);
+            digitalWrite(trigger, LOW);
 
             while (digitalRead(echo)==0)
             {
-                startTimer=0; //start time
+                startTimer=micros(); //start time
             }
             while (digitalRead(echo)==1)
             {
-                stoptimer=0; //stoptimer
+                stoptimer=micros(); //stoptimer
             }
             return ((stoptimer-startTimer)*34300)/2;
         }
-
 };
 
 int main(void){
+    wiringPiSetupGpio();
+
     Reifen R_Rechts(int);   
-    Reifen R_Links(int); 
+    Reifen R_Links(int);
+
+    Ultraschall AS_vorne_rechts(int, int);
+    Ultraschall AS_vorne_links(int, int);
+    Ultraschall AS_hinten_rechts(int, int);
+    Ultraschall AS_hinten_links(int, int);
 
     return 0;
 }
