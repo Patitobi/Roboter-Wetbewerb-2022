4f70d177c05b6e1c5b0f0fdaa654bbca4737d419
Patitobi <kesseltobias0@gmail.com>
2022-11-10 11:04:36 +0100

    Update main.cpp

--- WiringPi/RW2022/main.cpp
+++ WiringPi/RW2022/main.cpp
@@ -2,31 +2,28 @@
 #include <stdio.h>
 
 class IRsenden{
-    private:
-    int senderPin[3]= {1,1,1};
     public:
-    IRsenden(){
-        
-    }
-    void sendinfo(){
+        int senderPin[3]= {1,1,1};
+        IRsenden(){
+            
+        }
+        void sendinfo(){
 
-    }
+        }
 };
 class IRemfpaenger{
-    private:
-        int resiverPin[3] = {1,1,1};
     public:
-    IRemfpaenger(){
-    }
-    int getinfo(){
-        return 0;
-    }
+        int resiverPin[3] = {1,1,1};
+        IRemfpaenger(){
+        }
+        int getinfo(){
+            return 0;
+        }
 };
 class Reifen{
-    private:
+    public:
         int steuerpin[2] = {1, 1};
         bool mode;
-    public:
         Reifen(){
             mode=false;
 
@@ -123,40 +120,41 @@ class Ultraschall{
             //formal zum umrechnen von qS in mM
             float finDistance = ((time*343)/1000)/2;
         return finDistance;
-    }
-    int recordpulselength(){
-        //speicher der zeit bevor der recievepin aus 1 geht
-        starttime = micros();
-        while (digitalRead(recievepin) == HIGH){}
-        //speicher der zeit nachdem der recievepin aus 1 war
-        stoptime = micros();
-        //die differenz zwichen start und stop wird ausgegeben
-        return stoptime - starttime;
-    }
+        }
+        int recordpulselength(){
+            //speicher der zeit bevor der recievepin aus 1 geht
+            starttime = micros();
+            while (digitalRead(recievepin) == HIGH){}
+            //speicher der zeit nachdem der recievepin aus 1 war
+            stoptime = micros();
+            //die differenz zwichen start und stop wird ausgegeben
+            return stoptime - starttime;
+        }
 };
 class Auto: public Ultraschall, public Reifen, public IRemfpaenger, public IRsenden{
-    private:
+    protected:
     public:
+        bool run =true;
     Auto(){
+        if(!setup()) run=false;
+        
+        while (run){
+            
+        }
+    }
+    bool setup(){
+        if(wiringPiSetupGpio()) return true;
+        else return false;
+    }
+    void update(){
+
+    }
+    void execude(){
 
     }
 };
 int main(void){
-    wiringPiSetupGpio();
-
-    //Reifen R_Rechts(int);   
-    //Reifen R_Links(int);
-
-    Ultraschall Abstand_vorne_rechts;
-    //Ultraschall AS_vorne_links(int, int);
-    //Ultraschall AS_hinten_rechts(int, int);
-    //Ultraschall AS_hinten_links(int, int);
-    while (1){
-        printf("LoS!!!\n");
-        float durschdistanc = Abstand_vorne_rechts.get_durschnitliche_distanz(50 ,1);
-        printf("dursch %f\n", durschdistanc);
-        printf("Fehler = %d\n",Abstand_vorne_rechts.anzfehler);
-        delayMicroseconds(2000);
-    }
-    return 0;
+    Auto Roboter;
+    if (!Roboter.run) return 1;
+    else return 0;
 }
