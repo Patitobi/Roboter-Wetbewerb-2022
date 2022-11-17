cdcd2b6073bb18299d701cbbdfa163f8378accd4
Patitobi <kesseltobias0@gmail.com>
2022-11-17 22:58:35 +0100

    FarbSensor V-0.1

--- WiringPi/RW2022/main.cpp
+++ WiringPi/RW2022/main.cpp
@@ -131,6 +131,79 @@ class Ultraschall{
             return stoptime - starttime;
         }
 };
+class FarbSensor{
+    protected:
+    public:
+    int s0 =1;
+    int s1 =1;
+    int s2 =1;
+    int s3 =1;
+    int out0 =1;
+    int out1 =1;
+    int out2 =1;
+    int outarry[3]={out0, out1, out2};
+    // werte die zur umwantlung zu rgb gebraucht wird abhänig von helischkeit
+    int kalValue[3][2]={{0,1},{0,1},{0,1}};
+
+    // rgb werte von jedem sesor
+    // [0-2] sensor nummer
+    // [x][0] ist rot [x][1] ist gruen [x][2] ist blau
+    int colorValue[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
+    FarbSensor(){
+        // bestimmen der out und inputs
+        pinMode(s0, OUTPUT);
+        pinMode(s1, OUTPUT);
+        pinMode(s2, OUTPUT);
+        pinMode(s3, OUTPUT);
+        pinMode(out0, INPUT);
+        pinMode(out1, INPUT);
+        pinMode(out2, INPUT);
+        // setzt output frequency auf 20% ist gaube ich für die helischkeit der LEDs
+        digitalWrite(s0, HIGH);
+        digitalWrite(s1, LOW);
+
+        // noch keine function
+        kalibriren();
+    }
+    long map(long x, long in_min, long in_max, long out_min, long out_max) {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+    bool kalibriren(){
+        return true;
+    }
+    void updateSensorS(){
+        for (int sensor : outarry){
+            colorValue[sensor][0]=map(rot(sensor), kalValue[sensor][0], kalValue[sensor][1], 0, 255);
+            colorValue[sensor][1]=map(gruen(sensor), kalValue[sensor][0], kalValue[sensor][1], 0, 255);
+            colorValue[sensor][2]=map(blau(sensor), kalValue[sensor][0], kalValue[sensor][1], 0, 255);
+        }
+    } 
+    // die methode blau rot und gruen sind alle gleich bis auch die farb einstellung
+    int blau(int sensnum){
+        // einstellung ddes sensors auf die zu messene farbe
+        digitalWrite(s2, LOW);
+        digitalWrite(s3, HIGH);
+        //im besten fall solte alles so direckt klapen gehe aber nicht davon aus
+        // recordpulselength() hat vermutlich noch die falsche einstellung wird aber noch behoben in der schule
+        return recordpulselength(sensnum);
+    }
+    int rot(int sensnum){
+        digitalWrite(s2, LOW);
+        digitalWrite(s3, LOW);
+        return recordpulselength(sensnum);
+    }
+    int gruen(int sensnum){
+        digitalWrite(s2, HIGH);
+        digitalWrite(s3, HIGH);
+        return recordpulselength(sensnum);
+    }
+    int recordpulselength(int sensnum){
+        int starttime = micros();
+        while (digitalRead(sensnum) == LOW){}
+        int stoptime = micros();
+        return stoptime - starttime;
+    }
+};
 class Auto: public Ultraschall, public Reifen, public IRemfpaenger, public IRsenden{
     protected:
     // in [x][0] sind die neusten messungen in [x][1] die vorheringen und in [x][2] die differenz der beiden
@@ -150,8 +223,8 @@ class Auto: public Ultraschall, public Reifen, public IRemfpaenger, public IRsen
         }
     }
     bool setup(){
-        if(wiringPiSetupGpio()) return true;
-        else return false;
+        if(!wiringPiSetupGpio()) return false;
+        else return true;
     }
     void update(){
         updateUltaschall();
@@ -160,16 +233,21 @@ class Auto: public Ultraschall, public Reifen, public IRemfpaenger, public IRsen
 
     }
     void updateUltaschall(){
+        // mutipel arry mit 4 mal 3 int
+        // 0-4 steht für die sensor nummer [x]0-[x]3 sind die gemessenen werte
+        // 0 ist die zuletzt gemachte messung 
+        // 1 die dafor und 2 die geschwindigkeit der annäherung 
         for(int i=0;i<4;i++){
-            int distanceTimeStart=micros();
-            ultraschallsave[1][i] = ultraschallsave[0][i];
+            int distanceTimeStart=micros(); // zeit speichern um eine maximale dauer festzulegen
+            ultraschallsave[1][i] = ultraschallsave[0][i]; // speichert den alten wert mit den neusten
             do{
-                distance = get_durschnitliche_distanz(20, i);
-            }while (distance<=0||distanceTimeout<micros()-distanceTimeStart);
-            ultraschallsave[0][i] = distance;
-            if (ultraschallsave[i][0]<=0||ultraschallsave[i][1]<=0){
-                ultraschallsave[i][2] = -1;
-            } else ultraschallsave[i][2] = ultraschallsave[i][1]-ultraschallsave[i][0];
+                distance = get_durschnitliche_distanz(20, i); // mist die neue distanze
+            }while (distance<=0||distanceTimeout<micros()-distanceTimeStart); // nicht den timout überschritten oder error dann nochmal
+            ultraschallsave[0][i] = distance; // wenn distanz richtig gemessen oder timout wird der wert gespeichert ich -1 oder gemessene distanz
+            if (ultraschallsave[i][0]<=0||ultraschallsave[i][1]<=0){    // wenn einer der beiden werte -1 also falsch ist dann kann keine geschwindikeit bestimmt werden
+                ultraschallsave[i][2] = -1; // dan ist geschwindikeit auch -1 also falsch
+            } else ultraschallsave[i][2] = ultraschallsave[i][1]-ultraschallsave[i][0]; // wenn beide richtig dann normal
+            // geschwindikeit ist beher nur die defernz der beiden werte wird noch genauer ein wert zwichen 0 und 1 soll als indikator eingebaut werden
         }
     }
 };
