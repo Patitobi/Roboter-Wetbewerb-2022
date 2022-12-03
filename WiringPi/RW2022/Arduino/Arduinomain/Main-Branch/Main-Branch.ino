68d7bd3a985afaf6a6d7ffed36e8b93812e5de0b
DerTxmon <timon.kardel@gmail.com>
2022-12-03 21:40:52 +0100

    Listen to Ampel, Auto einrheiung(Ampel), Synch

--- WiringPi/RW2022/Arduino/Arduinomain/Main-Branch/Main-Branch.ino
+++ WiringPi/RW2022/Arduino/Arduinomain/Main-Branch/Main-Branch.ino
@@ -208,8 +208,12 @@ void GetIR(){
   if (irrecv.decode(&results)){ //Wenn irgendwas auf dem recvPin Gelesen wird dann 
       Serial.println(results.value, HEX); //Print in den Arduino Log (HEX)
       hexvalue = String(results.value, HEX); //Hier kurz eine variable machen um nicht in allen if statements die funktion durch zu führen (performance)
-      if(hexvalue.indexOf("0x11") > 0){ //Schaut ob 0x11 im String irgendwo vorhanden ist. Denn 0x11 sind die ersten 2 ziffern des ampel hex codes
-        AmpelPing(results.value);
+      if(hexvalue == 0x1101){ //Grünes zeichen von Ampel
+        AmpelPing(0x1101);
+      }else if(hexvalue == 0x1210){ //Ampel Anfahrt 
+        //Geb Signal nach hinten weiter
+        SendIR(0x1210, 2, 0)
+        AmpelAnfahrt(); //Fängt ann an den nächsten mann anzufahren (7cm) und wartet dann
       }
       irrecv.resume(); //Reset + es wird wieder vom Pin auf Info gewartet.
   }
@@ -231,14 +235,42 @@ void CodetoBeExecutedOnInterrupt(){
   GetIR();
 }
 void AmpelPing(long Code){
-  //Wenn dieses Auto erster in der Reihe dann reiche die Info nach hinten weiter.
+  //Wenn dieses Auto nicht letzter in der Reihe dann reiche die Info nach hinten weiter.
   if(NuminReihe != 5){
     //Pass IR Signal to the Cars behind
     SendIR(Code, 3, 0); //Send 3x hinter dich (0 =  nach hinten)
+    //Start driving forward
+    Fahrlos();
   }
+}
+void Fahrlos(){
+  Rreley(true);
+  Lreley(true);
+}
+void RedLineReached(){ //Muss von Farbsensor gecallt werden und kann auch nur von index 1 gecallt werden
+  //Ping hinter dich das die zu dir bis auf eine bestimmte distanz auffahren sollen und dann auch stehen bleiben.
+  //Dann Ping die Ampel an das die anfangen soll ihr Programm abzurfen -> (Ampel wartet 8 Sekunden und gibt dann grünes Signal via IR)
+  SendIR(0x1210, 2, 0); //Stehen bleiben 2x nach hinten
+  if(NuminReihe == 1) SendIR(0x1240, 2, 1); //If Abfrage nur zur Sicherheit. Eigentlich unnötig. //2. Vorderes Auto sendet Signal zur ampel damit Ampel anfängt zu agieren
+  //Warte nun auf Ampel Signal und gebe wenn Ampel Signal da das Signal an die hinteren weiter
+  hexvalue = 0; //einmal kurz vorher clearen
+  while(hexvalue != 0x1101){}
+  if(hexvalue == 0x1101){ //if zur sicherheit falls interrupt was durcheinander bringt
+    //send nach hinten weiter damit jeder los fährt
+    SendIR(0x1101, 3, 0);
+  }
+}
+void AmpelAnfahrt(){//Wird aufgerufen um dem vordermann bis auf 5cm aufzufahren und dann zu warten bis Grünes Licht signal vom vordermann kommt.
+  //Fahr so lange an bis was 7 cm vor deiner nase ist dann bleib stehen und warte einfach nur bis das "Ampel ist grün" Signal kommt.
+  Rreley(true);
+  Lreley(true);
+  while(entfernung[0] > 70){} //Warte bis auto vorne 7 cm nah ist
+  Rreley(false); //Stopp
+  Lreley(false);
 }
 void update(){
   updatesensor(0);
+  updatecolcor();
 }
 void machen(){
   USScheck();
